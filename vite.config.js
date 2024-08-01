3f5f94419f31f7982a3625dfb712921d988ed991
shaharyshahary <133163029+shah4ry@users.noreply.github.com>
2024-08-01 15:14:22 -0400

    added the weong website thing

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { ghPages } from 'vite-plugin-gh-pages'
 
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: '/' // Replace 'your-repo-name' with your GitHub repo name
+  build: {
+    outDir: 'dist',
+  },
 })
